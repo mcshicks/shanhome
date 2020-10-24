1efb5a46abf8f6065bcc97f204f31b441ef2e186
steve <steve@mcshicks@gmail.com>
2020-10-24 09:42:40 -0700

    Added MQTT support

--- mate_mqtt.py
+++ mate_mqtt.py
@@ -1,15 +1,19 @@
 # Forked from pyMate controller
 # Author: Stephen Hicks
 #
-# Allows communication with an Outback Systems MATE controller panel,
-# which provides diagnostic information of current charge state and power use
-#
-# Currently only supports the MX status page (from an Outback MX charge controller)
+# Allows communication with an Outback Systems MATE controller and
+# publshing values local using MQTT
+
+# Currently only supports the FX and MX status page 
 #
 # NOTE: This is intended for communication with the MATE's RS232 port, not Outback's proprietary protocol.
 
 import serial
 from value import Value
+import time
+import random
+import paho.mqtt.client as mqtt
+# import paho.mqtt.publish as publish
 
 
 class MXStatusPacket(object):
@@ -59,23 +63,8 @@ class FXStatusPacket(object):
         if chk_expected != chk_actual:
             raise Exception("Checksum error in received packet")       
 
+
 class MateCom(object):
-    """
-    Interfaces with the MATE controller on a specific COM port.
-    Must be a proper RS232 port with RTS/DTR pins.
-    """
-    def __init__(self, port, baudrate=19200):
-        self.ser = serial.Serial(port, baudrate, timeout=2)
-
-        # Provide power to the Mate controller
-        self.ser.setDTR(True)
-        self.ser.setRTS(False)
-
-        self.ser.readline()
-
-    def read_status(self):
-        ln = self.ser.readline().strip()
-        return MXStatusPacket(ln) if ln else None
     """
     Interfaces with the MATE controller on a specific COM port.
     Must be a proper RS232 port with RTS/DTR pins.
@@ -97,19 +86,47 @@ class MateCom(object):
             return "FX: ", FXStatusPacket(ln)
         else:
             return None, None
-     
+
+    def read_all(self):
+        fx = None
+        mx = None
+        fxfound = False
+        mxfound = False
+        while (not (fxfound and mxfound)):
+            if ln.startswith('C'):
+                mx = MXStatusPacket(ln)
+                mxfound = True
+            if ln.startswith('1'):
+                fx = FXStatusPacket(ln)
+                fxfound = True
+        return mx, fx
+    
+
     def read_raw(self):
         return self.ser.readline().strip()
-    
+
+
 if __name__ == "__main__":
-    # Test
+
+    broker = '127.0.0.1'
+    state_topic = 'home-assistant/battery/voltage'
+    delay = 60
+
+    client = mqtt.Client("ha-client")
+    client.connect(broker)
+    client.loop_start()
+    
+# Send a single message to set the mood
+# publish.single('home-assistant/fabian/mood', 'good', hostname=broker)
+
+# Send messages in a loop
+
     mate = MateCom('/dev/ttyUSB0')
-    for x in range(2):
-        type, status = mate.read_status()
-        print type + str(status.__dict__)
-    for x in range(2):
-        print mate.read_raw()
 
 
+while True:
+    mx, fx = mate.read_all()
+    client.publish(state_topic, random.randrange(0, 50, 1))
+    time.sleep(delay)
 
         
